717c8f643b89b434f3eccb5ca8351e07dd8a43d3
01musician <57641609@qq.com>
2024-07-20 06:19:13 +0800

    openresty: add example to use openresty.

---
+++ openresty/helloworld/lua/hello.lua
@@ -0,0 +1,4 @@
+local mod = require "testModule"
+ngx.say("hello, world")
+ngx.say(mod.color.green)
+
---
+++ openresty/helloworld/lua/testModule.lua
@@ -0,0 +1,10 @@
+local _M = {}
+
+_M.color = {
+	red = 1,
+	blue = 2,
+	green = 3
+}
+
+return _M
+
